ac8989a14b53633204832d057a292bb5cfb776d3
carpenaloza <car.penaloza@hotmail.es>
2021-08-30 09:27:36 -0400

    sesiones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_022110) do
+ActiveRecord::Schema.define(version: 2021_08_30_050303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,14 @@ ActiveRecord::Schema.define(version: 2021_08_26_022110) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "categoria", force: :cascade do |t|
+  create_table "administradores", force: :cascade do |t|
+    t.string "nombre_usuario"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categorias", id: :bigint, default: -> { "nextval('categoria_id_seq'::regclass)" }, force: :cascade do |t|
     t.string "categoria"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -62,5 +69,5 @@ ActiveRecord::Schema.define(version: 2021_08_26_022110) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "productos", "categoria"
+  add_foreign_key "productos", "categorias"
 end
