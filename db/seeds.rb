53a239049f1d361ba9913937945ffcd3f23b0ece
carpenaloza <car.penaloza@hotmail.es>
2021-09-01 10:13:45 -0400

    regiones

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,27 @@ TiposPago.create([
     {pago: 'efectivo'},
     {pago: 'tarjeta'},
 ])
+
+EstadosPedido.create([
+    {estado: 'solicitado'},
+    {estado: 'enviado'},
+    {estado: 'entregado'},
+])
+
+Region.create([
+    { nombre: 'Primera Región'},
+    { nombre: 'Segunda Región'},
+    { nombre: 'Tercera Región'},
+    { nombre: 'Cuarta Región'},
+    { nombre: 'Quinta Región'},
+    { nombre: 'Sexta Región'},
+    { nombre: 'Septima Región'},
+    { nombre: 'Octava Región'},
+    { nombre: 'Novena Región'},
+    { nombre: 'Décima Región'},
+    { nombre: 'Décima Primera Región'},
+    { nombre: 'Décima Cuarta Región'},
+    { nombre: 'Décima Quinta Región'},
+    { nombre: 'Décima Sexta Región'},
+    { nombre: 'Región Metropolitana'},
+])
