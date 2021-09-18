e343eec1ea5e0856c2e8854a6e2e6f3271060a24
carpenaloza <car.penaloza@hotmail.es>
2021-09-18 01:46:40 -0300

    diseño navbars

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,17 +39,26 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'localhost:3000',
+  #   user_name:            Rails.application.credentials.correo[:direccion],
+  #   password:             Rails.application.credentials.correo[:password],
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true
+  # }
+
+  config.action_mailer.default_url_options = { :host => 'shikit.best' }
+
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:               'smtp.zoho.com',
     port:                 587,
-    domain:               'localhost:3000',
     user_name:            Rails.application.credentials.correo[:direccion],
     password:             Rails.application.credentials.correo[:password],
     authentication:       'plain',
     enable_starttls_auto: true
   }
-
-
 
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'carrito.shikit.best' }
+  config.action_mailer.default_url_options = { :host => 'shikit.best' }
 
   config.action_mailer.smtp_settings = {
     address:               'smtp.zoho.com',
